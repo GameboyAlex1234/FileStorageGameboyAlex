461f9acb8d83d1191c12566f76f9f2e3e61604d2
GameboyAlex1234 <gameboyalex1234@gmail.com>
2026-06-06 20:40:28 +0200

    Update kofi-code.js

--- netlify/functions/kofi-code.js
+++ netlify/functions/kofi-code.js
@@ -1,7 +1,7 @@
 const { google } = require("googleapis");
 const { Resend } = require("resend");
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY2);
 
 exports.handler = async (event) => {
   try {
